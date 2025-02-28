7d89ac05d5f11d0c7c492c898bb617647c9b21ec
aleksandr <sanek.v.07@gmail.com>
2025-02-28 23:53:08 +0300

    add userAuth Entity

---
+++ app/src/main/java/com/kotleters/mobile/feature/auth/domain/UserAuth.kt
@@ -0,0 +1,15 @@
+package com.kotleters.mobile.feature.auth.domain
+
+sealed class UserAuth {
+    data class Client(
+        val firstName: String,
+        val email: String,
+        val password: String
+    ) : UserAuth()
+
+    data class Company(
+        val name: String,
+        val email: String,
+        val password: String
+    ) : UserAuth()
+}
