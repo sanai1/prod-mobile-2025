435f6cda36f9276d5d0295905b156acd36e86af7
igordudka <i.dudka007@gmail.com>
2025-03-01 01:30:53 +0300

    added colors

--- app/src/main/java/com/kotleters/mobile/app/MainActivity.kt
+++ app/src/main/java/com/kotleters/mobile/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kotleters.mobile.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,7 +11,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(0),
+            navigationBarStyle = SystemBarStyle.dark(0)
+        )
         setContent {
 
         }
