a7ec0209da98ea40d4ec213b9bccb3c17a437f3d
igordudka <i.dudka007@gmail.com>
2025-03-01 13:17:58 +0300

    add UI components (top header & bottom bar)

--- app/src/main/java/com/kotleters/mobile/app/MainActivity.kt
+++ app/src/main/java/com/kotleters/mobile/app/MainActivity.kt
@@ -5,7 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.kotleters.mobile.R
 import com.kotleters.mobile.common.navigation.AppNavigation
+import com.kotleters.mobile.common.navigation.BottomBarScreen
+import com.kotleters.mobile.common.ui.components.BottomBar
+import com.kotleters.mobile.common.ui.components.TopScreenHeader
+import com.kotleters.mobile.common.ui.theme.backgroundColor
 import com.kotleters.mobile.feature.auth.presentation.login.LoginScreen
 import com.kotleters.mobile.feature.auth.presentation.onboard.AuthOnBoardScreen
 import com.kotleters.mobile.feature.auth.presentation.register.client.ClientRegisterScreen
@@ -21,7 +38,29 @@ class MainActivity : ComponentActivity() {
             navigationBarStyle = SystemBarStyle.dark(0)
         )
         setContent {
-            AppNavigation()
+            var screen by remember {
+                mutableStateOf("Главная")
+            }
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .background(backgroundColor)
+                    .statusBarsPadding()
+            ) {
+                TopScreenHeader(
+                    screen
+                )
+                Spacer(Modifier.weight(1f))
+                BottomBar(
+                    listOf(
+                        BottomBarScreen("Главная", R.drawable.home),
+                        BottomBarScreen("Оплата", R.drawable.pay)
+                    ),
+                    screen = screen
+                ) {
+                    screen = it.route
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/kotleters/mobile/common/navigation/BottomBarScreen.kt
@@ -0,0 +1,6 @@
+package com.kotleters.mobile.common.navigation
+
+data class BottomBarScreen (
+    val route: String,
+    val icon: Int,
+)
