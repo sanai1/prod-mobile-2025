25f3bdd695f8871c783c60b5567a81df3528a160
igordudka <i.dudka007@gmail.com>
2025-03-01 18:39:23 +0300

    pay screen

---
+++ app/src/main/java/com/kotleters/mobile/feature/company/presentation/pay/CompanyPayScreen.kt
@@ -0,0 +1,9 @@
+package com.kotleters.mobile.feature.company.presentation.pay
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CompanyPayScreen(modifier: Modifier = Modifier) {
+    
+}
