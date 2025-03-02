506c8698d94e03cf6ecba969bc5845e88258abc3
aleksandr <sanek.v.07@gmail.com>
2025-03-02 05:00:36 +0300

    add updateToken in StatisticRepositoryImpl

--- app/src/main/java/com/kotleters/mobile/feature/company/data/repository/StatisticRepositoryImpl.kt
+++ app/src/main/java/com/kotleters/mobile/feature/company/data/repository/StatisticRepositoryImpl.kt
@@ -3,13 +3,16 @@ package com.kotleters.mobile.feature.company.data.repository
 import android.content.Context
 import com.kotleters.mobile.common.data.network.model.ResponseTemplate
 import com.kotleters.mobile.common.data.network.model.SecretStorage
+import com.kotleters.mobile.feature.auth.domain.UserAuth
+import com.kotleters.mobile.feature.auth.domain.UserAuthRepository
 import com.kotleters.mobile.feature.company.data.network.client.StatisticRetrofitClient
 import com.kotleters.mobile.feature.company.data.network.model.StatisticModelMapper
 import com.kotleters.mobile.feature.company.domain.entity.Statistic
 import com.kotleters.mobile.feature.company.domain.repository.StatisticRepository
 
 class StatisticRepositoryImpl(
-    private val context: Context
+    private val context: Context,
+    private val userAuthRepository: UserAuthRepository
 ) : StatisticRepository {
     override suspend fun getStatistic(): ResponseTemplate<List<Statistic>> {
         try {
@@ -19,6 +22,7 @@ class StatisticRepositoryImpl(
                     data = StatisticModelMapper.toStatisticList(call.body()!!)
                 )
             } else if (call.code() == 401) {
+                updateToken()
                 val callAgain = getStatisticRetrofit()
                 return if (callAgain.code() == 200) {
                     ResponseTemplate.Success(
@@ -40,4 +44,15 @@ class StatisticRepositoryImpl(
     private fun getStatisticRetrofit() = StatisticRetrofitClient.statisticRetrofitService.getAllStatistic(
         token = getToken()
     ).execute()
+
+    private suspend fun updateToken() {
+        val triple = SecretStorage.readPassAndEmail(context)
+        userAuthRepository.auth(
+            userAuth = UserAuth.Company(
+                name = null,
+                email = triple.first!!,
+                password = triple.second!!
+            )
+        )
+    }
 }
