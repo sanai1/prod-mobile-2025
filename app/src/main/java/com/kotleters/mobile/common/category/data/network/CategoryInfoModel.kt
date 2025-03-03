38bf6974cb75b184388a5c56e8cb467b8486f78e
aleksandr <sanek.v.07@gmail.com>
2025-03-03 19:06:27 +0300

    add CategoryInfoRepository

---
+++ app/src/main/java/com/kotleters/mobile/common/category/data/network/CategoryInfoModel.kt
@@ -0,0 +1,7 @@
+package com.kotleters.mobile.common.category.data.network
+
+data class CategoryInfoModel(
+    val id: Long,
+    val category: String,
+    val subcategory: String
+)
---
+++ app/src/main/java/com/kotleters/mobile/common/category/domain/CategoryInfo.kt
@@ -0,0 +1,7 @@
+package com.kotleters.mobile.common.category.domain
+
+data class CategoryInfo(
+    val id: Long,
+    val category: String,
+    val subcategory: String
+)
