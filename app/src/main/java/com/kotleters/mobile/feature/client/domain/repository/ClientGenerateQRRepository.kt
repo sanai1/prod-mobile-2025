4e0c42a5891d1f1a1adeb10cf9e5ec04888dc6b3
aleksandr <sanek.v.07@gmail.com>
2025-03-04 05:28:42 +0300

    add query in ClientGenerateQRRepository

--- app/src/main/java/com/kotleters/mobile/feature/client/domain/repository/ClientGenerateQRRepository.kt
+++ app/src/main/java/com/kotleters/mobile/feature/client/domain/repository/ClientGenerateQRRepository.kt
@@ -3,5 +3,5 @@ package com.kotleters.mobile.feature.client.domain.repository
 import com.kotleters.mobile.common.data.network.model.ResponseTemplate
 
 interface ClientGenerateQRRepository {
-    suspend fun clientGenerateQRRepository(offerId: String): ResponseTemplate<String>
+    suspend fun clientGenerateQRRepository(offerId: String, spendBonus: Boolean): ResponseTemplate<String>
 }
